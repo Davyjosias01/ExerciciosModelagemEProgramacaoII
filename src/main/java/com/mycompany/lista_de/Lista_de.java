ed74ffaab00327b789f6ca2445b2452506564294
Davy <davy.josias@gmail.com>
2025-03-16 19:50:06 -0300

    First commit

---
+++ src/main/java/com/mycompany/lista_de/Lista_de.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lista_de;
+
+/**
+ *
+ * @author davyj
+ */
+public class Lista_de {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
